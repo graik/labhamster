f9ef17dfc288e803717bfe06ba216a25d2b52fc7
Raik Grünberg <graik@users.noreply.github.com>
2012-06-19 19:49:37 +0000

    bugfix: incomplete search specification was breaking search in Order Table

--- labhamster/admin.py
+++ labhamster/admin.py
@@ -91,7 +91,7 @@ class OrderAdmin(admin.ModelAdmin):
                    'item__category__name', 'item__vendor__name', 'created_by')
     ordering = ('-date_created', 'item', 'quantity')
 
-    search_fields = ('comment', 'grant')
+    search_fields = ('comment', 'grant__name', 'grant__grant_id')
 
     save_as = True
 
