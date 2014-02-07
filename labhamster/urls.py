fa5fb564b609751d30e6472ba7ef5d1d3441195e
Raik Grünberg <graik@users.noreply.github.com>
2014-02-07 20:18:10 +0000

    migration to django 1.6

---
+++ labhamster/urls.py
@@ -0,0 +1,24 @@
+## Copyright 2012 Raik Gruenberg
+
+## This file is part of the labhamster project (http://labhamster.sf.net)
+## Labhamster is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Affero General Public License as
+## published by the Free Software Foundation, either version 3 of the
+## License, or (at your option) any later version.
+
+## Labhamster is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Affero General Public License for more details.
+
+## You should have received a copy of the GNU Affero General Public
+## License along with labhamster. If not, see <http://www.gnu.org/licenses/>.
+
+
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+##urlpatterns = patterns('', url('^admin/', include(admin.site.urls)))
+urlpatterns = patterns('', url('', include(admin.site.urls)))
