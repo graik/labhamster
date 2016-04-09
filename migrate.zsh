8204c337c8d6d48f00affc84ca0985fd73802b2b
graik <graik@users.noreply.github.com>
2016-04-09 21:11:16 +0300

    file cleanup

--- migrate.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-export DJANGO_SITENAME=labhamsterdjango
-export DJANGO_SETTINGS_MODULE=labhamsterdjango.settings
-
-python manage.py schemamigration labhamster --auto
-
-python manage.py migrate labhamster
--- resetdb.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-
-python manage.py dumpdata --exclude auth --exclude contenttypes > datadump.json
-
-python manage.py reset getitfast
-
-python manage.py syncdb
-
-python manage.py loaddata datadump.json
