d39692ff06cc131d5c7e38d0f7dde40eba953db0
graik <graik@users.noreply.github.com>
2016-04-09 16:47:26 +0300

    re-organize templates; activate custom base/index

--- labhamstersite/settings.py
+++ labhamstersite/settings.py
@@ -68,14 +68,20 @@ WSGI_APPLICATION = 'labhamstersite.wsgi.application'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+                 os.path.join(BASE_DIR, 'labhamster', 'templates'),
+                 os.path.join(BASE_DIR, 'site_templates'),
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages"
             ],
         },
     },
