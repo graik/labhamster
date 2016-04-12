53b7a190d51e1b5a4039f1a0c76ffd574a346e4b
graik <graik@users.noreply.github.com>
2016-04-12 17:23:00 +0300

    temporarily remove secret key

--- labhamstersite/settings_production.py
+++ labhamstersite/settings_production.py
@@ -20,7 +20,7 @@ RUNNING_DEV_SERVER = ('runserver' in sys.argv)
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'your-secret-key'
 try:
 	## prepare env variable:
 	## heroku config:add DJANGO_SECRET_KEY="your_secret_key"
